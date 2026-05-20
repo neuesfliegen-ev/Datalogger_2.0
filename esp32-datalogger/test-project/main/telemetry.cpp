c1da875ec64d729b4978dc43a580886467e75f6d
jin <pearlisrich@naver.com>
2026-05-20 17:49:52 +0200

    comments: telemetry.h and telemetry.cpp

--- esp32-datalogger/test-project/main/telemetry.cpp
+++ esp32-datalogger/test-project/main/telemetry.cpp
@@ -1,16 +1,30 @@
 #include <cstdint>
 #include "telemetry.h"
 
+/* Methods */
 
-esp_err_t Telemetry::update_telemetry(const IMUData& imuData){
-	dataset.ax = imuData.acc.af[0]; dataset.ay = imuData.acc.af[1]; dataset.az = imuData.acc.af[2];
-	dataset.gx = imuData.gyr.wf[0]; dataset.gy = imuData.gyr.wf[1]; dataset.gz = imuData.gyr.wf[2];  
-	dataset.hx = imuData.mag.h[0]; dataset.hy = imuData.mag.h[1]; dataset.hz = imuData.mag.h[2];  
-	dataset.roll = imuData.att.Attf[0]; dataset.pitch = imuData.att.Attf[1]; dataset.yaw = imuData.att.Attf[2];
-	dataset.tmp = imuData.tmp.tmpf; 
-	dataset.hght = imuData.bar.lHeight; dataset.press = imuData.bar.lPressure;
+// update_telemetry
+// This method takes in the data from the sensors, and updates the dataset struct with the new data.
+esp_err_t Telemetry::update_telemetry(const IMUData &imuData)
+{
+	// 1. IMU data
+	dataset.ax = imuData.acc.af[0];
+	dataset.ay = imuData.acc.af[1];
+	dataset.az = imuData.acc.af[2];
+	dataset.gx = imuData.gyr.wf[0];
+	dataset.gy = imuData.gyr.wf[1];
+	dataset.gz = imuData.gyr.wf[2];
+	dataset.hx = imuData.mag.h[0];
+	dataset.hy = imuData.mag.h[1];
+	dataset.hz = imuData.mag.h[2];
+	dataset.roll = imuData.att.Attf[0];
+	dataset.pitch = imuData.att.Attf[1];
+	dataset.yaw = imuData.att.Attf[2];
+	dataset.tmp = imuData.tmp.tmpf;
+	dataset.hght = imuData.bar.lHeight;
+	dataset.press = imuData.bar.lPressure;
 
-	
+	/* Other sensor data should be updated here */
 
 	return ESP_OK;
 }
--- esp32-datalogger/test-project/main/telemetry.h
+++ esp32-datalogger/test-project/main/telemetry.h
@@ -1,37 +1,46 @@
 #include "esp_check.h"
 #include "imu_data.h"
 
-struct SDataset {
+/* Structs */
+
+// Struct to save all three sensor data in one struct.
+struct SDataset
+{
 	uint32_t t;
 	float ax, ay, az;
 	float gx, gy, gz;
 	float hx, hy, hz;
 	float roll, pitch, yaw;
 	float tmp;
-	float hght, press; 
+	float hght, press;
 	float psta, pdyn;
 	float alt, gs, lat, lon;
 };
 
-struct SDataset_raw {
+// Struct to save all data, in short (resistor level)
+// In resistor level, the data is in short, nd needs to be converted to float
+// using the conversion factors provided in the datasheets of the sensors.
+struct SDataset_raw
+{
 	short ax, ay, az;
 	short gx, gy, gz;
 	short hx, hy, hz;
 	short roll, pitch, yaw;
 	short tmp;
-	short hght; 
+	short hght;
 	float psta, pdyn;
 	float alt, gs, lat, lon;
 };
 
-class Telemetry{
+/* Classes */
+// Telemetry class to save all the data in one struct, and update the data from the sensors.
+class Telemetry
+{
 public:
-	Telemetry(){};
+	Telemetry() {};
 	struct SDataset dataset;
 	struct SDataset rawDataset;
-	esp_err_t update_telemetry(const IMUData& imuData);
+	esp_err_t update_telemetry(const IMUData &imuData);
+
 private:
-
 };
-
-
